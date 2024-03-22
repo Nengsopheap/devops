46ac5e87e71572d6d49beae04ba68ca7f4485234
Nengsopheap <realpheap8@gmail.com>
2024-03-22 17:59:50 +0700

    second commit

---
+++ src/test/java/com/tp01/tp01/Tp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp01.tp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
